5b4ae51a191727112549c04bb46a8ae145f6dcfd
Templars <antoine.ratat@gmail.com>
2020-11-17 14:31:50 +0800

    yup mixed custom validation for file size and format, upload picture function

--- src/components/form/FormUser.js
+++ src/components/form/FormUser.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import {
@@ -6,10 +6,9 @@ import {
 	Card,
 	CardBody,
 	FormGroup,
+	FormText,
 	Form,
 	Input,
-	InputGroupAddon,
-	InputGroupText,
 	InputGroup,
 	Col,
 	Row,
@@ -19,32 +18,83 @@ import {
 
 //INFO: FUNCTION FORM USER
 export default function FormUser() {
+	// useState Hooks
+	const [imageLoaded, setImageLoaded] = useState(false)
+	const [imageDesc, setImageDesc] = useState('')
+	const [imageTitle, setImageTitle] = useState('')
+	const [profilePicture, setProfilePicture] = useState({
+		name: '',
+		size: '',
+		type: '',
+	})
+
 	// Validation schema with Yup
 	const regexNoSpecial = /^[a-zA-Z. ]*$/
+	const supported_format = ['image/jpg', 'image/jpeg', 'image/gif', 'image/png']
 	const validationSchema = Yup.object({
+		profile_picture: Yup.mixed()
+			.test('fileSize', 'File is too large', (value) => !value || value.size <= 2500000)
+			.test(
+				'fileType',
+				'Format is wrong',
+				(value) => !value || supported_format.includes(value.type)
+			),
 		image_title: Yup.string()
-			.min(2, 'Title too short')
-			.max(20, 'Title too long')
-			.matches(regexNoSpecial, "Title doesn't contain numbers or special characters")
+			.min(2, 'Too short')
+			.max(20, 'Too long')
+			.matches(regexNoSpecial, 'No numbers or special characters')
 			.required('Required'),
 		image_desc: Yup.string()
-			.min(2, 'Description too short')
-			.max(50, 'Description too long')
-			.matches(regexNoSpecial, "Description doesn't contain numbers or special characters")
+			.min(2, 'Too short')
+			.max(50, 'Too long')
+			.matches(regexNoSpecial, 'No numbers or special characters')
 			.required('Required'),
 	})
 
 	// Handle Form with Formik
-	const { handleSubmit, handleChange, handleBlur, values, touched, errors } = useFormik({
+	const {
+		handleSubmit,
+		handleChange,
+		handleBlur,
+		setFieldValue,
+		values,
+		touched,
+		errors,
+	} = useFormik({
 		initialValues: {
+			file: '',
 			image_title: '',
 			image_desc: '',
 		},
 		validationSchema,
 		onSubmit(values) {
-			console.log(values)
+			setImageTitle(values.image_title)
+			setImageDesc(values.image_desc)
+			console.log(values.profile_picture)
+			setProfilePicture({
+				name: values.profile_picture.name,
+				size: values.profile_picture.size,
+				type: values.profile_picture.type,
+			})
+			setImageLoaded(true)
+			uploadPicture(values)
 		},
 	})
+
+	function uploadPicture(values) {
+		let data = new FormData()
+		console.log(values.profile_picture)
+		data.append('profile_picture', values.profile_picture)
+		return fetch('http://localhost:5000/api/user/setting', {
+			method: 'POST',
+			headers: new Headers({
+				Accept: 'application/json',
+			}),
+			body: data,
+		})
+			.then((response) => response.json())
+			.catch((error) => console.log(error))
+	}
 
 	return (
 		<>
@@ -59,7 +109,7 @@ export default function FormUser() {
 								<Form onSubmit={handleSubmit}>
 									{/* image_title */}
 									<FormGroup className='mb-3'>
-										<Label>Image title</Label>
+										<Label className='label_form'>Image title</Label>
 										<InputGroup className='input-group-alternative'>
 											<Input
 												id='image_title'
@@ -78,7 +128,7 @@ export default function FormUser() {
 
 									{/* image_desc */}
 									<FormGroup className='mb-3'>
-										<Label>Image description</Label>
+										<Label className='label_form'>Image description</Label>
 										<InputGroup className='input-group-alternative'>
 											<Input
 												id='image_desc'
@@ -95,12 +145,50 @@ export default function FormUser() {
 										)}
 									</FormGroup>
 
+									{/* upload picture */}
+									<FormGroup>
+										<Label for='exampleFile'>Profile picture</Label>
+										<Input
+											type='file'
+											name='profile_picture'
+											id='profile_picture'
+											onBlur={handleBlur}
+											onChange={(event) => {
+												setFieldValue(
+													'profile_picture',
+													event.currentTarget.files[0]
+												)
+											}}
+										/>
+										<FormText color='muted'>
+											Supported format .png .jpg .jpeg (maximum size: 1Mo).
+										</FormText>
+										{errors.profile_picture && touched.profile_picture && (
+											<div className='error_field'>
+												{errors.profile_picture}
+											</div>
+										)}
+									</FormGroup>
+
 									{/* submit form */}
 									<Button className='my-4' color='primary' type='submit'>
 										Send
 									</Button>
 								</Form>
 							</CardBody>
+							{imageLoaded && (
+								<CardBody>
+									Image Title: {imageTitle}
+									<br />
+									Image Desc: {imageDesc}
+									<br />
+									Profile Picture: {profilePicture.name}
+									<br />
+									Size: {profilePicture.size}Ko
+									<br />
+									Type: {profilePicture.type}
+								</CardBody>
+							)}
 						</Card>
 					</Col>
 				</Row>
